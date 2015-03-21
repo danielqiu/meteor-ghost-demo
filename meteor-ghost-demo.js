6c5746c4efbc69fb7bb02d8f5e264692a6bfadf4
Daniel Qiu <daniel@danielqiu.com>
2015-03-20 22:25:13 -0700

    initial commit

---
+++ meteor-ghost-demo.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    ghost().then(function (ghostServer) {
+      ghostServer.start();
+    });
+  });
+}
