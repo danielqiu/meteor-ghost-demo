706c81f99d7b500c02859e285ba99c2d00a64374
Daniel Qiu <daniel@danielqiu.com>
2015-04-05 22:30:56 -0700

    supports custom configuration and themes

--- meteor-ghost-demo.js
+++ meteor-ghost-demo.js
@@ -2,9 +2,22 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
   Meteor.startup(function () {
     // code to run on server at startup
-    ghost().then(function (ghostServer) {
+
+    var path = Npm.require('path');
+    var __dirname = process.env.PWD;
+
+    ghost({
+  		config: path.join(__dirname, 'config.js')
+	   });
+
+	  ghost().then(function (ghostServer) {
+
+      //var config = ghostServer.config;
+      //console.log(config);
+
       ghostServer.start();
     });
   });
